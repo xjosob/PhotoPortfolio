2ce40db6419a987cf48e769aec6deb341d6edd15
xjosob <jocke.s@windowslive.com>
2024-06-12 17:50:48 +0200

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,19 @@
-// Controllers/HomeController.cs
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PhotoPortfolio.Data;
 
-namespace PhotoPortfolio.Controllers
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ApplicationDbContext _context;
+
+    public HomeController(ApplicationDbContext context)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var albums = _context.Albums.Include(a => a.Photos).ToList();
+        return View(albums);
     }
 }
-
