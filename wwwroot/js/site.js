f1ecea008a21507f5d773137e1e03548d5e36f0c
xjosob <jocke.s@windowslive.com>
2024-06-19 12:58:53 +0200

    update

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(function () {
+    $('img[data-toggle="modal"]').click(function () {
+        var src = $(this).attr('src');
+        $('#modalImage').attr('src', src);
+        $('#imageModal').modal('show');
+    });
+});
