552c85a1d8802986f55b92c57ac7238b5c785768
xjosob <jocke.s@windowslive.com>
2024-06-19 14:35:12 +0200

    update

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,5 +1,5 @@
 ﻿$(document).ready(function () {
-    $('img[data-toggle="modal"]').click(function () {
+    $('.img-fluid').click(function () {
         var src = $(this).attr('src');
         $('#modalImage').attr('src', src);
         $('#imageModal').modal('show');
